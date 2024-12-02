02825edab41721bdcc0c7bb700bedad464bb10bf
ahmadalanib <alnaib888@gmail.com>
2024-12-02 10:18:02 +0100

    add test17

--- src/FeatureFlagsServiceProvider.php
+++ src/FeatureFlagsServiceProvider.php
@@ -17,7 +17,7 @@ class FeatureFlagsServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/database/migrations/2024_01_01_create_feature_flags_table.php' => database_path('migrations/2024_01_01_create_feature_flags_table.php'),
         ], 'feature-flags-migrations');
-
+    }
     public function register()
     {
         $this->app->singleton('feature-flags', function() {
