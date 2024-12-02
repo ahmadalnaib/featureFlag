8698c2b50ffb0d5a0fb25ceaa74b51fe2c76f5bb
ahmadalanib <alnaib888@gmail.com>
2024-12-02 13:10:17 +0100

    add test 123

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 <?php
 use App\Http\Controllers\FeatureFlagController;
 
-Route::middleware(['web', 'auth:admin'])->prefix('admin')->group(function () {
+
     Route::get('/features', [FeatureFlagController::class, 'index']);
     Route::post('/features', [FeatureFlagController::class, 'store']);
     Route::put('/features/{id}', [FeatureFlagController::class, 'update'])
