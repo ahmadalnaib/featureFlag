10bd0b68ea27adbfc84309969f5b4c60dd4ad341
ahmadalanib <alnaib888@gmail.com>
2024-12-02 09:54:25 +0100

    test4

--- src/FeatureFlagsServiceProvider.php
+++ src/FeatureFlagsServiceProvider.php
@@ -1,40 +1,45 @@
 <?php
+
 namespace YourName\LaravelFeatureFlags;
 
 use Illuminate\Support\ServiceProvider;
 
-class FeatureFlagsServiceProvider extends ServiceProvider
+class FeatureFlagServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        
-      // Publish configuration file
-      $this->publishes([
-        __DIR__.'/../config/feature-flags.php' => config_path('feature-flags.php'),
-    ], 'feature-flags-config');
+        // Load migrations from the package
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
-    // Publish migrations
-    $this->publishes([
-        __DIR__.'/../database/migrations/' => database_path('migrations')
-    ], 'feature-flags-migrations');
+        // Load routes from the package
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
-    // Publish views
-    $this->publishes([
-        __DIR__.'/../resources/views' => resource_path('views/vendor/feature-flag')
-    ], 'feature-flags-views');
+        // Load views from the package
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'feature-flag');
 
-    // Publish controllers (if needed)
-    $this->publishes([
-        __DIR__.'/../Http/Controllers' => app_path('Http/Controllers/Vendor/FeatureFlag')
-    ], 'feature-flags-controllers');
+        // Publish configuration file
+        $this->publishes([
+            __DIR__.'/../config/feature-flags.php' => config_path('feature-flags.php'),
+        ], 'feature-flags-config');
+
+        // Publish migrations
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'feature-flags-migrations');
+
+        // Publish views
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/feature-flag')
+        ], 'feature-flags-views');
+
+        // Publish controllers (if needed)
+        $this->publishes([
+            __DIR__.'/../Http/Controllers' => app_path('Http/Controllers/Vendor/FeatureFlag')
+        ], 'feature-flags-controllers');
     }
 
     public function register()
     {
-        $this->app->singleton('feature-flags', function() {
-            return new FeatureFlags();
-        });
+        //
     }
 }
