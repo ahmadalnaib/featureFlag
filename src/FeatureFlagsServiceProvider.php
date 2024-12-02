74ecb4032006137320601630e11e923c76aaee53
ahmadalanib <alnaib888@gmail.com>
2024-12-02 09:48:38 +0100

    edit 2

--- src/FeatureFlagsServiceProvider.php
+++ src/FeatureFlagsServiceProvider.php
@@ -10,9 +10,25 @@ class FeatureFlagsServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         
-        $this->publishes([
-            __DIR__.'/config/feature-flags.php' => config_path('feature-flags.php'),
-        ], 'feature-flags-config');
+      // Publish configuration file
+      $this->publishes([
+        __DIR__.'/../config/feature-flags.php' => config_path('feature-flags.php'),
+    ], 'feature-flags-config');
+
+    // Publish migrations
+    $this->publishes([
+        __DIR__.'/../database/migrations/' => database_path('migrations')
+    ], 'feature-flags-migrations');
+
+    // Publish views
+    $this->publishes([
+        __DIR__.'/../resources/views' => resource_path('views/vendor/feature-flag')
+    ], 'feature-flags-views');
+
+    // Publish controllers (if needed)
+    $this->publishes([
+        __DIR__.'/../Http/Controllers' => app_path('Http/Controllers/Vendor/FeatureFlag')
+    ], 'feature-flags-controllers');
     }
 
     public function register()
