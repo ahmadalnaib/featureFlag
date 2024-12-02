257c3309832bf21d340c9caf9c795d75787f17f3
ahmadalanib <alnaib888@gmail.com>
2024-12-02 13:10:54 +0100

    add test 77

--- src/FeatureFlagsServiceProvider.php
+++ src/FeatureFlagsServiceProvider.php
@@ -13,31 +13,16 @@ class FeatureFlagsServiceProvider extends ServiceProvider
         
         $this->publishes([
             __DIR__.'/config/feature-flags.php' => config_path('feature-flags.php'),
-        ], 'feature-flags-config');
-     
-        $this->publishes([
             __DIR__.'/database/migrations/2024_01_01_create_feature_flags_table.php' => database_path('migrations/2024_01_01_create_feature_flags_table.php'),
-        ], 'feature-flags-migrations');
-   
-        $this->publishes([
             __DIR__.'/resources/js/Pages/Admin/Features/Index.vue' => resource_path('js/Pages/Admin/Features/Index.vue'),
-        ], 'feature-flags-views');
-
-        $this->publishes([
             __DIR__.'/Models/FeatureFlag.php' => app_path('Models/FeatureFlag.php'),
-        ], 'feature-flags-models');
-
-        $this->publishes([
             __DIR__.'/Http/Controllers/FeatureFlagController.php' => app_path('Http/Controllers/FeatureFlagController.php'),
-        ], 'feature-flags-controllers');
-
-        $this->publishes([
             __DIR__.'/Http/Controllers/Api/FeatureFlagController.php' => app_path('Http/Controllers/Api/FeatureFlagController.php'),
-        ], 'feature-flags-controllers-api');
-
-
-        
+            __DIR__.'/routes/web.php' => base_path('routes/web.php'),
+            __DIR__.'/routes/api_v1.php' => base_path('routes/api.php'),
+        ], 'feature-flags');
     }
+
     public function register()
     {
         $this->app->singleton('feature-flags', function() {
