da79379ee0aab2fbd01397eb2c353bf6552076e2
ahmadalanib <alnaib888@gmail.com>
2024-12-03 13:48:53 +0100

    add

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\FeatureFlagController;
 
 
 Route::prefix('admin')->group(function () {
-    Route::get('/features', [FeatureFlagController::class, 'index']);
+ 
     Route::post('/features', [FeatureFlagController::class, 'store']);
     Route::put('/features/{id}', [FeatureFlagController::class, 'update']);
 });
